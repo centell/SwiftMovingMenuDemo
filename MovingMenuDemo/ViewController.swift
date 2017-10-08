8682ec70200f81a5285d43e4eb6fa7ab79f84f21
Centell <centell@me.com>
2017-10-08 22:53:00 +0900

    1.0.0 first commit

--- MovingMenuDemo/ViewController.swift
+++ MovingMenuDemo/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuLeadingContraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openBtn(_ sender: UIButton) {
+        menuLeadingContraint.constant = 0
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
+        animateMenuMoving()
     }
 
-
+    @IBAction func closeBtn(_ sender: Any) {
+        menuLeadingContraint.constant = -300
+        menuView.layer.shadowOpacity = 0
+        menuView.layer.shadowRadius = 0
+        animateMenuMoving()
+    }
+    
+    
+    func animateMenuMoving() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,6 @@
+#  MovingMenu Demo
+======
+![Swift](https://img.shields.io/badge/Swift-4.0-orange.svg)
+
+A simple menu on/off example using Swift.
+Click Open to bring up the menu.
